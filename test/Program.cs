d11212cb23257f3a30078ef7c6b864be889e8333
yassineghannay <109186232+yassineghannay@users.noreply.github.com>
2024-01-14 19:52:02 -0700

    final (hopefully)

--- test/Program.cs
+++ test/Program.cs
@@ -1,13 +1,13 @@
 
 
 // Task 1 && 2 
-Console.WriteLine("Please enter a low number : ");
-
-
 //Getting user input and then using Convert.ToInt32 in order to do calculations 
+Console.WriteLine("Please enter a low number : ");
 var lowNum1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please enter a High number : ");
+
+Console.WriteLine("Please enter a high number : ");
 var highNum1 = Convert.ToInt32(Console.ReadLine());
+
 
 
 // While loop that is true if the first input is not negative AND it is lower then second input( highNum1)
@@ -20,6 +20,5 @@ while (lowNum1 > 0 && highNum1 > lowNum1)
     highNum1 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine(lowNum1 - highNum1);
 }
-
 
 
